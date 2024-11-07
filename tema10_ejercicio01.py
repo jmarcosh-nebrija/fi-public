2fcd3916024d67d657687b68f6d92bc6374d92d1
Jose Marcos <jose.marcos@ext.esa.int>
2024-11-07 09:33:26 +0100

    Añade ejercicio tema10

---
+++ tema10_ejercicio01.py
@@ -0,0 +1,30 @@
+def recorta(lista):
+    """Elimina el primer y el último elemento de una lista si existen.
+
+    Parameters
+    ----------
+    lista : list
+        La lista con elementos a modificar
+
+    Raises
+    -------
+    IndexError
+        Si la lista no tiene al menos 2 elementos.
+    """
+
+
+
+def centro(lista):
+    """Elimina el primer y el último elemento de una lista si existen.
+
+    Parameters
+    ----------
+    lista : list
+        La lista con elementos a modificar
+
+    Returns
+    -------
+    list
+        Una copia de la lista de entrada sin el primer y el último elemento.
+    """
+
